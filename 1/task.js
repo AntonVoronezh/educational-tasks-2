c5138f872af99224c4a74b9c5dd4aa7bcda331e1
AntonVoronezh <pstut@mail.ru>
2018-09-10 19:14:47 +0300

    1 #4

--- 1/task.js
+++ 1/task.js
@@ -2,14 +2,13 @@ import { length, toUpperCase, toLowerCase } from './strings'; // eslint-disable-
 
 // BEGIN (write your solution here)
 const invertCase = (str) => {
-
   let out = '';
   let i = 0;
 
-  while (length(str)-1 >= i) {
+  while (length(str) - 1 >= i) {
     if (str[i] === toUpperCase(str[i])) {
       out += toLowerCase(str[i]);
-    } else if (str[i] === toLowerCase(str[i])){
+    } else if (str[i] === toLowerCase(str[i])) {
       out += toUpperCase(str[i]);
     } else {
       out += str[i];
