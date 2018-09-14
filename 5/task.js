e61b592015a51b72ae12c7f0708fde1446f5e7da
AntonVoronezh <pstut@mail.ru>
2018-09-14 18:34:19 +0300

    5 #5

---
+++ 5/task.js
@@ -0,0 +1,5 @@
+import cube from './numbers1';//x
+import multi, { pow2 as pow, sub2 as sub, sum2 as sum } from './numbers2';
+import sqrt from './numbers3';//x
+
+export { pow, sum, sub, sqrt, multi, cube as default };
