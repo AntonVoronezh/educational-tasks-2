e3a2ffa93275ac47b8a3bbdf33a75dd69314440b
AntonVoronezh <pstut@mail.ru>
2018-09-15 18:37:37 +0300

    6 #6

--- 6/task.js
+++ 6/task.js
@@ -1,17 +1,15 @@
 import { length, substr } from './strings';
 
-// BEGIN (write your solution here)
 const reverse = (str) => {
   let i = str.length - 1;
   let result = '';
 
   while (i >= 0) {
-    result = result + str[i];
-    i = i - 1;
+    result += str[i];
+    i -= 1;
   }
 
   return result;
 };
 
 export default reverse;
-// END
