b415b71fbd3964e569faa8a9b45c650615a241a5
AntonVoronezh <pstut@mail.ru>
2018-09-17 19:00:42 +0300

    8 #5

---
+++ 8/task.js
@@ -0,0 +1,20 @@
+const diff = (a, b) => {
+  let result = 0;
+  let x = 0;
+  let y = 0;
+
+  if (a >= b){
+    x = a - b;
+  } else {
+    x = b - a;
+  }
+
+  if (360 - x > x) {
+    result = x;
+  } else {
+    result = 360 - x;
+  }
+
+  return result;
+};
+
