c92b14731e3cbfbd40d4bfda3b5d840c10150069
AntonVoronezh <pstut@mail.ru>
2018-09-17 19:10:34 +0300

    9 #6

---
+++ 9/task.js
@@ -0,0 +1,14 @@
+const ackermann = (m, n) => {
+  let out = 0;
+  if (m >= 0 && n >= 0) {
+    if (m === 0) {out = n + 1;}
+    if (m > 0) {
+      if (n === 0) {out = ackermann(m - 1, 1);}
+      if (n > 0) {out = ackermann(m - 1, ackermann(m, n - 1));}
+    }
+    console.log(out);
+    return out; 
+  }
+};
+
+export default ackermann;
