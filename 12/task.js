3f96b421d85cafef8c320f304fa58cc401a643a6
AntonVoronezh <pstut@mail.ru>
2018-09-19 18:27:42 +0300

    12 #4

--- 12/task.js
+++ 12/task.js
@@ -1,28 +1,24 @@
 const areBracketsBalanced = (str) => {
   let levo = 0;
   let pravo = 0;
-  let i = 0;
   let out = 0;
-  
+
   if (length(str) % 2 === 0) {
-        for (let i = 0; i < length(str); i += 1) {
-          if (String(str[i]) === '(') {
-            out += 1;
-            levo += 1;
-          }
-          if (String(str[i]) === ')') {
-            pravo += 1;
-            out -= 1;
-          }
-  
-            if (levo < pravo ) {return false;}
-        }
-       return levo === pravo && out === 0 ? true : false;
-      
+    for (let i = 0; i < length(str); i += 1) {
+      if (String(str[i]) === '(') {
+        out += 1;
+        levo += 1;
+      }
+      if (String(str[i]) === ')') {
+        pravo += 1;
+        out -= 1;
+      }
+
+      if (levo < pravo) { return false; }
     }
+    return !!(levo === pravo && out === 0);
+  }
   return false;
-    
-  } 
-  
-  export default areBracketsBalanced;
-  
+};
+
+export default areBracketsBalanced;
