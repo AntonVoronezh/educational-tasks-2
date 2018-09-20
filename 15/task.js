eeeafe52d4c93fbd20fe141b2b3611b160fe712e
AntonVoronezh <pstut@mail.ru>
2018-09-20 18:42:09 +0300

    15 #6

--- 15/task.js
+++ 15/task.js
@@ -1,24 +1,18 @@
 import { getLength } from './strings';
 
-// BEGIN (write your solution here)
 const substr = (str, begin = 0, end = getLength(str)) => {
+  if (begin < 0) { begin = 0; }
+  if (end === 0) { return ''; }
+  if (end < 0) { end = 1; }
+  if (begin > getLength(str)) { return ''; }
+  end += begin;
+  if (begin + end > getLength(str)) { end = getLength(str); }
 
-
-//Отрицательное число
-if (begin < 0) {begin = 0;}
-if (end === 0) {return '';}
-if (end < 0) {end = 1;}
-if (begin > getLength(str)) {return '';}
-end = end + begin;
-if (begin + end > getLength(str)) {end = getLength(str);}
-
-console.log(begin + '-' + end);
-
-let i = 0;
-let out = '';
+  let out = '';
   for (begin; begin < end; begin += 1) {
     out += String(str[begin]);
-  } 
-return out;
-}
+  }
+  return out;
+};
+
 export default substr;
